85e22781fef95ae9410cb18b02521feb892d0b9c
Peter Kiraly <peter.kiraly@gwdg.de>
2016-10-19 13:34:42 +0200

    Issue #41: fixing ID truncation bug.

---
+++ src/test/java/de/gwdg/metadataqa/api/calculator/TruncatedIDTest.java
@@ -0,0 +1,87 @@
+package de.gwdg.metadataqa.api.calculator;
+
+import de.gwdg.metadataqa.api.counter.FieldCounter;
+import de.gwdg.metadataqa.api.model.JsonPathCache;
+import de.gwdg.metadataqa.api.schema.EdmOaiPmhXmlSchema;
+import de.gwdg.metadataqa.api.util.FileUtils;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Péter Király <peter.kiraly at gwdg.de>
+ */
+public class TruncatedIDTest {
+
+	JsonPathCache cache;
+	FieldExtractor calculator;
+
+	public TruncatedIDTest() {
+	}
+
+	@BeforeClass
+	public static void setUpClass() {
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+	}
+
+	@Before
+	public void setUp() throws URISyntaxException, IOException {
+		calculator = new FieldExtractor(new EdmOaiPmhXmlSchema());
+		String jsonContent = FileUtils.readFirstLine("issue-examples/issue41-truncatedID.json");
+		cache = new JsonPathCache(jsonContent);
+	}
+
+	@After
+	public void tearDown() {
+	}
+
+	@Test
+	public void truncation() {
+		calculator.measure(cache);
+		String csv = calculator.getCsv(true, true);
+		assertEquals("\"recordId\":9200365/BibliographicResource_3000059507130,\"dataset\":9200365_Ag_EU_TEL_a0142_Gallica,\"dataProvider\":National Library of France", csv);
+		assertEquals(
+			"9200365/BibliographicResource_3000059507130",
+			calculator.getResultMap()
+				.get(calculator.FIELD_NAME)
+		);
+		assertEquals(3, calculator.getLabelledResultMap().get(calculator.getCalculatorName()).size());
+		assertEquals(
+			"9200365/BibliographicResource_3000059507130", 
+			calculator
+				.getLabelledResultMap()
+					.get(calculator.getCalculatorName())
+						.get(calculator.FIELD_NAME)
+		);
+		assertEquals(
+			"9200365/BibliographicResource_3000059507130", 
+			calculator
+				.getLabelledResultMap()
+					.get(calculator.getCalculatorName())
+						.get("recordId")
+		);
+		assertEquals(
+			"National Library of France", 
+			calculator
+				.getLabelledResultMap()
+					.get(calculator.getCalculatorName())
+						.get("dataProvider")
+		);
+		assertEquals(
+			"9200365_Ag_EU_TEL_a0142_Gallica", 
+			calculator
+				.getLabelledResultMap()
+					.get(calculator.getCalculatorName())
+						.get("dataset")
+		);
+	}
+}
