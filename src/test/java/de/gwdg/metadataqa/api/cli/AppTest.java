8332ff596479c034ab2a319439d21fdd9396f37e
Péter Király <pkiraly@gwdg.de>
2022-11-20 18:01:27 +0100

    Testing App

--- src/test/java/de/gwdg/metadataqa/api/cli/AppTest.java
+++ src/test/java/de/gwdg/metadataqa/api/cli/AppTest.java
@@ -28,11 +28,30 @@ public class AppTest {
   }
 
   @Test
-  public void main() throws IOException {
+  public void good_yaml() throws IOException {
     App.main(new String[]{
       "--input", BASE_DIR + "csv/meemoo-simple.csv",
       "--schema", BASE_DIR + "configuration/schema/simple-meemoo.yaml",
       "--measurements", BASE_DIR + "configuration/measurement/simple-meemoo.yaml",
+      "--outputFormat", "csv",
+      "--output", outputFileName,
+    });
+
+    assertTrue(outputFile.exists());
+
+    List<String> output = FileUtils.readLinesFromFile(outputFileName);
+    assertEquals(3, output.size());
+    assertEquals("\"url\",\"name\"", output.get(0).trim());
+    assertEquals("\"https://neurovault.org/images/384958/\",\"massivea uditory lexical decision\"", output.get(1).trim());
+    assertEquals("\"https://neurovault.org/images/93390/\",\"Language in the aging brain\"", output.get(2).trim());
+  }
+
+  @Test
+  public void good_json() throws IOException {
+    App.main(new String[]{
+      "--input", BASE_DIR + "csv/meemoo-simple.csv",
+      "--schema", BASE_DIR + "configuration/schema/simple-meemoo.json",
+      "--measurements", BASE_DIR + "configuration/measurement/simple-meemoo.json",
       "--outputFormat", "csv",
       "--output", outputFileName,
     });
