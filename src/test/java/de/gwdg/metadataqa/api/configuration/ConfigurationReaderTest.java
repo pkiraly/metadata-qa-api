1a71c5932404fd93e01101bf1af6658c10019f16
Péter Király <pkiraly@gwdg.de>
2023-12-08 10:46:10 +0100

    Implement SonarCloud quality suggestions #159

--- src/test/java/de/gwdg/metadataqa/api/configuration/ConfigurationReaderTest.java
+++ src/test/java/de/gwdg/metadataqa/api/configuration/ConfigurationReaderTest.java
@@ -1,14 +1,21 @@
 package de.gwdg.metadataqa.api.configuration;
 
+import com.jayway.jsonpath.InvalidJsonException;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.io.FileNotFoundException;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class ConfigurationReaderTest {
+
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
 
   @Test
   public void readSchemaJson() {
@@ -27,6 +34,17 @@ public class ConfigurationReaderTest {
   }
 
   @Test
+  public void readNonexistentConfiguration() throws FileNotFoundException {
+    thrown.expect(FileNotFoundException.class);
+    thrown.expectMessage("src/test/resources/configuration/measurement/non-existent-configuration.json (No such file or directory)");
+
+    MeasurementConfiguration configuration = ConfigurationReader
+      .readMeasurementJson("src/test/resources/configuration/measurement/non-existent-configuration.json");
+
+    fail("Should throw an exception if the JSON file is not existent.");
+  }
+
+    @Test
   public void readMeasurementYaml() throws FileNotFoundException {
     MeasurementConfiguration configuration = ConfigurationReader
       .readMeasurementYaml("src/test/resources/configuration/measurement/configuration.yaml");
