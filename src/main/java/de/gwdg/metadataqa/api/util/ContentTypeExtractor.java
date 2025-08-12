0ba58de09189676a25f2bcbf4d78be3fb4a03909
Péter Király <pkiraly@gwdg.de>
2025-08-12 20:22:53 +0200

    Improve content type extraction

--- src/main/java/de/gwdg/metadataqa/api/util/ContentTypeExtractor.java
+++ src/main/java/de/gwdg/metadataqa/api/util/ContentTypeExtractor.java
@@ -5,11 +5,14 @@ import org.apache.commons.lang3.StringUtils;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class ContentTypeExtractor {
   private static final Logger LOGGER = Logger.getLogger(ContentTypeExtractor.class.getCanonicalName());
   public static final int DEFAULT_TIMEOUT = 5000;
+  private static final List<String> HEADER_KEYS = List.of("Content-Type", "content-type");
+
   private int timeout;
 
   public ContentTypeExtractor() {
@@ -30,7 +33,8 @@ public class ContentTypeExtractor {
     urlConnection.connect();
     int responseCode = urlConnection.getResponseCode();
     if (responseCode == 200) {
-      String rawContentType = urlConnection.getHeaderField("Content-Type");
+      // String rawContentType = extractContentType.getHeaderField("Content-Type");
+      String rawContentType = extractContentType(urlConnection);
       if (rawContentType != null && StringUtils.isNotBlank(rawContentType))
         contentType = rawContentType.replaceAll("; ?charset.*$", "");
     } else if (responseCode == 301 // Moved Permanently
@@ -48,4 +52,13 @@ public class ContentTypeExtractor {
     return contentType;
   }
 
+  private String extractContentType(HttpURLConnection urlConnection) {
+    String contentType = null;
+    for (String key : HEADER_KEYS) {
+      contentType = urlConnection.getHeaderField(key);
+      if (StringUtils.isNotBlank(contentType))
+        break;
+    }
+    return contentType;
+  }
 }
